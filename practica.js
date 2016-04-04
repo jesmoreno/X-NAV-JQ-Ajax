984cfc5eed39036761c2d342b103d3cd5581666c
Jesus Jose Moreno Pinto <jesusjm@zeta40.aulas.gsyc.es>
2016-04-04 14:28:11 +0200

    práctica acabada

--- practica.js
+++ practica.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function() {
   $("#boton").click(function(){
 
     $.ajax({
-      url: "doc.txt"
+      url: "doc.txt",
+      cache: false
     })
     .done(function( data ) {
       $('#text').html(data);
